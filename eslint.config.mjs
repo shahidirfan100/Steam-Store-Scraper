fb23b75a8cd7f5fe2f6a85ef0bc5b85538a54fc1
Shahid Irfan <shahidirfan100@hotmail.com>
2025-12-18 18:35:31 +0500

    Refactor Docker setup and clean up project files
    Moved Dockerfile and related configuration to .actor directory, updated actor.json to reference new README path, and removed obsolete Dockerfile, .dockerignore, .gitignore, and ESLint config. Updated dependencies in package.json and removed development dependencies and scripts for a leaner production setup.

--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import prettier from 'eslint-config-prettier';
-
-import apify from '@apify/eslint-config/js.js';
-
-// eslint-disable-next-line import/no-default-export
-export default [{ ignores: ['**/dist'] }, ...apify, prettier];
